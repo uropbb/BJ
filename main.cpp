3cbcf0e8745fbe607d187bde42fa4f369e146991
Igor <uropb_b@mail.ru>
2020-07-25 11:36:42 +0300

    les3 card class created

---
+++ main.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+
+using namespace std;
+
+class Card {
+ private:
+//     enum suit;
+//     enum value;
+     bool pos;
+ public:
+    void Flip() {this->pos=!pos;}
+
+    int GetValue() {}
+};
+int main()
+{
+
+    return 0;
+}
